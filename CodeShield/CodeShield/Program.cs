419e765099738cff189b6a3c219f42bb226feb13
hendrix-llouchi <159572091+hendrix-llouchi@users.noreply.github.com>
2026-06-26 19:12:52 +0000

    Add OSV.dev vulnerability checking for npm and NuGet packages, fix BOM parsing bug, closes #9

--- CodeShield/CodeShield/Program.cs
+++ CodeShield/CodeShield/Program.cs
@@ -8,6 +8,7 @@ var builder = WebApplication.CreateBuilder(args);
 // Add services to the container.
 builder.Services.AddControllersWithViews();
 builder.Services.AddHttpClient<IGitHubService, GitHubService>();
+builder.Services.AddHttpClient<IOsvService, OsvService>();
 
 
 // Register ApplicationDbContext
